a2abc85674bba49735ac853d065d1504743aa57f
MohammadKaifSaiyad <saiyedkgn6@gmail.com>
2023-12-03 14:27:29 +0530

    about us is maybe done

--- AboutUs.js
+++ AboutUs.js
@@ -5,13 +5,32 @@ document.getElementById('theme-logo').addEventListener('click',() =>{
         document.documentElement.style.setProperty('--theme-color','white')
         document.documentElement.style.setProperty('--card-text-color','black')
         document.documentElement.style.setProperty('--card-background-color','rgba(255, 255, 255, 0.5)')
+        document.documentElement.style.setProperty('--body-text-color','black')
         document.getElementById('theme-logo').className = 'fa-solid fa-moon'
     }
     else{
         document.documentElement.style.setProperty('--theme-color','#37474f')
         document.documentElement.style.setProperty('--card-text-color','white')
         document.documentElement.style.setProperty('--card-background-color','rgba(82, 109, 130, 0.7)')
+        document.documentElement.style.setProperty('--body-text-color','white')
         document.getElementById('theme-logo').className = 'fa-solid fa-sun fa-xl'
     }
     
 })
+
+
+var words = ['Thanks', 'for', 'visiting', 'our', 'website.'];
+var area = document.getElementById('dynamic-text');
+let wordIndex = 0; 
+const typeEffect = ()=>{
+    const currentWord = words[wordIndex];
+    area.textContent +=currentWord+" ";
+    wordIndex++;
+    if(wordIndex >= 6){
+        area.textContent = "";
+        wordIndex = 0;
+    }
+    setTimeout(typeEffect, 500);
+};
+
+typeEffect();
